98c1c1ee65f9d7ed04f6e04320ed4adebbc9c14c
amit_pandey <amitvedprakashpandey@gmail.com>
2023-10-23 00:18:36 +0530

    Add React-Icons in PuchBox

--- src/Components/PunchBox.jsx
+++ src/Components/PunchBox.jsx
@@ -1,5 +1,10 @@
+import {BiUser} from 'react-icons/bi'
 export default function PunchBox() {
     return<>
-    <div className="bg-blue-700 h-36 shadow-lg p-3 m-2 rounded-md"></div>
+    <div className="bg-blue-700 h-36 shadow-lg p-3 m-2 rounded-md">
+        <div>
+            <BiUser/>
+        </div>
+    </div>
     </>
 }
